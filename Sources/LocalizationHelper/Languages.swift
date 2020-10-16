e4bb8387f7076b6e9e933b4e153bf65e95889d4e
Solovey42 <solovey.1999@mail.ru>
2020-10-16 18:45:50 +0700

    Create russian, english and portuguese language structures

--- Sources/LocalizationHelper/Languages.swift
+++ Sources/LocalizationHelper/Languages.swift
@@ -3,3 +3,18 @@
 //
 
 import Foundation
+
+struct Russian {
+    let key = "rus"
+    var Words: [String: String] = ["hello": "Привет", "day": "День"]
+}
+
+struct English {
+    let key = "en"
+    var Words: [String: String] = ["hello": "Hello", "day": "Day", "terms": "Terms"]
+}
+
+struct Portuguese {
+    let key = "rus"
+    var Words: [String: String] = ["day": "Dia", "terms": "Termos"]
+}
