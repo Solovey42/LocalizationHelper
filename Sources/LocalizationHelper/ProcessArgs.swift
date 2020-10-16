8439441fb56f3c4970bcc57b5d5849c205f271e2
Solovey42 <solovey.1999@mail.ru>
2020-10-16 17:53:06 +0700

    Create algorithms for search keys

--- Sources/LocalizationHelper/ProcessArgs.swift
+++ Sources/LocalizationHelper/ProcessArgs.swift
@@ -4,5 +4,16 @@
 
 import Foundation
 
-class ProcessArgs {
+public class ProcessArgs{
+    let arg:Array<String> = []
+    var language = ""
+    var key = ""
+    init (Arg:Array<String>){
+        if (Arg.contains("-l")){
+            language = Arg[Arg.firstIndex(of: "-l") ?? +1]
+        }
+        if (Arg.contains("-k")){
+            key = Arg[Arg.firstIndex(of: "-k") ?? +1]
+        }
+    }
 }
