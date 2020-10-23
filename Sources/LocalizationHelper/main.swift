a66081282ba843701a07a0acb94ead1dbec92275
Solovey42 <solovey.1999@mail.ru>
2020-10-23 17:59:20 +0700

    Fixed initialization ProccessArgs for working with Argument Parser

--- Sources/LocalizationHelper/main.swift
+++ Sources/LocalizationHelper/main.swift
@@ -2,16 +2,20 @@ import ArgumentParser
 
 struct CharacterCount: ParsableCommand {
 
-    @Option(name: .short,  help: "The number to multiply the count against.") var key: String = ""
+    @Option(name: .short, help: "The number to multiply the count against.") var key: String = ""
     @Option(name: .short, help: "The number to multiply the count against.") var language: String = ""
 
     func run() throws {
         let languages = [Russian(), English(), Portuguese()] as [Language]
-        let process = ProcessArgs.init(ArgArray: CommandLine.arguments)
+        let process = ProcessArgs.init(stringKey: key, stringLanguage: language)
         app(process: process, languages: languages)
     }
 }
 
 CharacterCount.main()
 
-
+/*
+let languages = [Russian(), English(), Portuguese()] as [Language]
+let process = ProcessArgs.init(ArgArray: CommandLine.arguments)
+app(process: process, languages: languages)
+*/
