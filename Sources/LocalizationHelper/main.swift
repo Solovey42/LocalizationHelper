6e3b0d7084f1ab0f190630d47f9e69a02672c5a3
Solovey42 <solovey.1999@mail.ru>
2020-10-23 17:45:40 +0700

    Rebuild main for working with ArgumentParser

--- Sources/LocalizationHelper/main.swift
+++ Sources/LocalizationHelper/main.swift
@@ -1,2 +1,17 @@
-let languages = [Russian(), English(), Portuguese()] as [Language]
-app(Arg: CommandLine.arguments, Languages: languages)
+import ArgumentParser
+
+struct CharacterCount: ParsableCommand {
+
+    @Option(name: .short,  help: "The number to multiply the count against.") var key: String = ""
+    @Option(name: .short, help: "The number to multiply the count against.") var language: String = ""
+
+    func run() throws {
+        let languages = [Russian(), English(), Portuguese()] as [Language]
+        let process = ProcessArgs.init(ArgArray: CommandLine.arguments)
+        app(process: process, languages: languages)
+    }
+}
+
+CharacterCount.main()
+
+
