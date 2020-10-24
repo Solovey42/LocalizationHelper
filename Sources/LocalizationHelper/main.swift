9eb69f412972881b27d2310a7dcc6369061a469d
solo <solovey.1999@mail.ru>
2020-10-25 01:32:53 +0700

    Fixed bugs

--- Sources/LocalizationHelper/main.swift
+++ Sources/LocalizationHelper/main.swift
@@ -1,17 +1,4 @@
-import ArgumentParser
+import Foundation
 
-struct Arguments: ParsableCommand {
 
-    @Option(name: .short, help: "The word to translate into.") var key: String = ""
-    @Option(name: .short, help: "Language into which we translate") var language: String = ""
-
-    func run() throws {
-        let languages = [Russian(), English(), Portuguese()] as [Language]
-        //let process = ProcessArgs.init(ArgArray: CommandLine.arguments) # using for read Commandline
-        let process = ProcessArgs.init(stringKey: key, stringLanguage: language)
-        app(process: process, languages: languages)
-    }
-}
-
-Arguments.main()
 
