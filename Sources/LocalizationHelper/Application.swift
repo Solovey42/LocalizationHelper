6142c30b4a128b48bef727cc43e431342db09ee1
Solovey42 <solovey.1999@mail.ru>
2020-10-24 17:22:55 +0700

    Code style

--- Sources/LocalizationHelper/Application.swift
+++ Sources/LocalizationHelper/Application.swift
@@ -23,7 +23,7 @@ func app(process: ProcessArgs, languages: Array<Language>) {
 func getKeys(languages: Array<Language>) -> Array<String> {
     var keys: Array<String> = []
     for i in 0...languages.count - 1 {
-        for (key, _) in languages[i].Words {
+        for (key, _) in languages[i].words {
             if keys.contains(key) == false {
                 keys.append(key)
             }
@@ -46,7 +46,7 @@ func printWithOutArg(keys: Array<String>, languages: Array<Language>) {
     for item in keys {
         print(item)
         for n in 0...languages.count - 1 {
-            for (key, value) in languages[n].Words
+            for (key, value) in languages[n].words
                 where item == key {
                 print("\t\(languages[n].key) = \(value)")
             }
@@ -61,7 +61,7 @@ func printWithLanguage(process: ProcessArgs, languagesKeys: Array<String>, langu
     }
     for i in 0...languages.count - 1 {
         if languages[i].key == process.language {
-            for (key, value) in languages[i].Words {
+            for (key, value) in languages[i].words {
                 print("\(key) = \(value)")
             }
         }
@@ -75,8 +75,8 @@ func printWithKey(process: ProcessArgs, keys: Array<String>, languages: Array<La
     }
     print(process.key)
     for i in 0...languages.count - 1 {
-        if languages[i].Words.keys.contains(process.key) {
-            for (key, value) in languages[i].Words
+        if languages[i].words.keys.contains(process.key) {
+            for (key, value) in languages[i].words
                 where key == process.key {
                 print("\t\(languages[i].key): \(value)")
                 break
@@ -92,7 +92,7 @@ func printWitAllArg(process: ProcessArgs, languagesKeys: Array<String>, language
     }
 
     for i in 0...languages.count - 1 {
-        for (key, value) in languages[i].Words
+        for (key, value) in languages[i].words
             where key == process.key && languages[i].key == process.language {
             print("\(value)")
             exit(0)
