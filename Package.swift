3f25ff3625d0bc1c60c3b3a0c5d2553bf3927410
solo <solovey.1999@mail.ru>
2020-10-25 01:48:55 +0700

    Fixed bugs

--- Package.swift
+++ Package.swift
@@ -8,12 +8,14 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-argument-parser", .upToNextMinor(from: "0.3.0")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "LocalizationHelper",
+                dependencies: [.product(name: "ArgumentParser", package: "swift-argument-parser")],
             resources: [.process("languages.json")]),
         .testTarget(
             name: "LocalizationHelperTests",
--- Sources/LocalizationHelper/main.swift
+++ Sources/LocalizationHelper/main.swift
@@ -1,4 +1,17 @@
 import Foundation
+import ArgumentParser
 
+struct Arguments: ParsableCommand {
 
+    @Option(name: .short, help: "The word to translate into.") var key: String = ""
+    @Option(name: .short, help: "Language into which we translate") var language: String = ""
 
+    func run() throws {
+        let languages = [Russian(), English(), Portuguese()] as [Language]
+        //let process = ProcessArgs.init(ArgArray: CommandLine.arguments) # using for read Commandline
+        let process = ProcessArgs.init(stringKey: key, stringLanguage: language)
+        app(process: process, languages: languages)
+    }
+}
+
+Arguments.main()
