f2ef7c356516d99f90a80dc5ec545303cfbb63a3
solo <solovey.1999@mail.ru>
2020-10-25 16:53:44 +0700

    Refactor decoding .json file

--- Sources/LocalizationHelper/main.swift
+++ Sources/LocalizationHelper/main.swift
@@ -9,17 +9,11 @@ struct Arguments: ParsableCommand {
         let path = Bundle.module.path(forResource: "languages", ofType: "json") ?? "languages.json"
         var languages = [] as [Language]
         let decoder = JSONDecoder()
-
         if let json = FileManager.default.contents(atPath: path) {
-            let curLanguage = (try? decoder.decode(Language.self, from: json))!
-            languages.append(curLanguage)
+            languages = try! JSONDecoder().decode([Language].self, from: json)
         } else {
             languages = []
         }
-
-        print(languages)
-
-        //let languages = [Russian(), English(), Portuguese()] as [Language]
         //let process = ProcessArgs.init(ArgArray: CommandLine.arguments) # using for read Commandline
         let process = ProcessArgs.init(stringKey: key, stringLanguage: language)
         app(process: process, languages: languages)
