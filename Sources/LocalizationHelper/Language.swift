e18603272946b6061419685bfc1a20ce5cd755f0
solo <solovey.1999@mail.ru>
2020-10-25 15:50:28 +0700

    Add decoding json file

--- Sources/LocalizationHelper/Language.swift
+++ Sources/LocalizationHelper/Language.swift
@@ -8,3 +8,7 @@ public protocol Language {
     var key: String { get }
     var words: [String: String] { get }
 }
+public struct LanguagE:Language,Codable {
+    public var key: String
+    public var words: [String: String]
+}
--- Sources/LocalizationHelper/main.swift
+++ Sources/LocalizationHelper/main.swift
@@ -6,7 +6,23 @@ struct Arguments: ParsableCommand {
     @Option(name: .short, help: "Language into which we translate") var language: String = ""
 
     func run() throws {
-        let languages = [Russian(), English(), Portuguese()] as [Language]
+        let path = Bundle.main.path( forResource: "languages", ofType: "json") ?? "123"
+        let url = Bundle.main.path(forResource: "languages", ofType: "json")
+        var languages = [] as [LanguagE]
+        let decoder = JSONDecoder()
+
+        if let json = FileManager.default.contents(atPath: path) {
+            let Curlanguage = (try? decoder.decode(LanguagE.self, from: json))!
+            languages.append(Curlanguage)
+        } else {
+            languages = []
+        }
+
+        print(path)
+        print(url as Any)
+        print(languages)
+
+        //let languages = [Russian(), English(), Portuguese()] as [Language]
         //let process = ProcessArgs.init(ArgArray: CommandLine.arguments) # using for read Commandline
         let process = ProcessArgs.init(stringKey: key, stringLanguage: language)
         app(process: process, languages: languages)
