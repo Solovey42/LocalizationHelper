d327861b95da4a01c741223f55ed10a44422d7ea
solo <solovey.1999@mail.ru>
2020-10-28 16:27:47 +0700

    Add flag for update and delete

--- Sources/LocalizationHelper/main.swift
+++ Sources/LocalizationHelper/main.swift
@@ -4,6 +4,8 @@ import ArgumentParser
 struct Arguments: ParsableCommand {
 
     @Flag(help: "Start search") var search: Bool = false
+    @Flag(help: "Update selected item") var update: Bool = false
+    @Flag(help: "Delete selected item") var delete: Bool = false
     @Option(name: .shortAndLong, help: "The word to translate into.") var key: String = ""
     @Option(name: .shortAndLong, help: "Language into which we translate") var language: String = ""
 
@@ -18,6 +20,15 @@ struct Arguments: ParsableCommand {
         let process = ProcessArgs.init(stringKey: key, stringLanguage: language)
         if search == true{
             app(process: process, languages: languages)
+        }
+        if update == true{
+
+        }
+        if delete == true{
+
+        }
+        else{
+            print(" -h, --help Show help information.")
         }
 
     }
