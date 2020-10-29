af6ae7b7fa5fde95ebaf3bf61dd9ad0154fcc940
solo <solovey.1999@mail.ru>
2020-10-29 22:43:42 +0700

    Add function get .json file in main

--- Sources/LocalizationHelper/main.swift
+++ Sources/LocalizationHelper/main.swift
@@ -45,41 +45,13 @@ extension Arguments{
             print("Delete")
         }
     }
+
+func getJson(path: String) throws -> [Language] {
+    var languages = [] as [Language]
+    if let json = FileManager.default.contents(atPath: path) {
+        try languages = JSONDecoder().decode([Language].self, from: json)
+    }
+    return languages
 }
-
-/*extension Arguments{
-    struct Search: ParsableCommand {
-
-        @Flag(help: "Start search") var search: Bool = false
-        @Flag(help: "Update selected item") var update: Bool = false
-        @Flag(help: "Delete selected item") var delete: Bool = false
-        @Argument var word: String?
-        @Option(name: .shortAndLong, help: "The word to translate into.") var key: String = ""
-        @Option(name: .shortAndLong, help: "Language into which we translate") var language: String = ""
-
-        func run() throws {
-
-            let path = Bundle.module.path(forResource: "languages", ofType: "json") ?? "languages.json"
-            var languages = [] as [Language]
-            if let json = FileManager.default.contents(atPath: path) {
-                try languages = JSONDecoder().decode([Language].self, from: json)
-            }
-            //let process = ProcessArgs.init(ArgArray: CommandLine.arguments) //using for read Commandline
-            let process = ProcessArgs.init(stringKey: key, stringLanguage: language)
-            if search == true {
-                app(process: process, languages: languages)
-            } else if update == true {
-
-            } else if delete == true {
-
-            } else {
-                print(Arguments.helpMessage())
-            }
-        }
-    }
-    struct Update{
-
-    }
-}*/
 
 Arguments.main()
