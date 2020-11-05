35e5b7eeb54c14ecb8446b364fda865288e5c922
solo <solovey.1999@mail.ru>
2020-11-05 19:51:17 +0700

    Fix logic for start function search, update, and delete

--- Sources/LocalizationHelper/main.swift
+++ Sources/LocalizationHelper/main.swift
@@ -17,16 +17,21 @@ let arguments = container.argumentParser.parsing()
 let getterData = container.getData
 var data = try getterData.gettingData()
 
-switch arguments{
+if case .search(let key?, let language?) = arguments {
+    let process = ProcessArgs(stringConfig: "search", stringKey: key, stringLanguage: language)
+    try search(languages: &data, process: process) // не передавать весь класс , а передать отдельные аргументы и создать из функций классы с интерфейсами
+}
+
+/*switch arguments{
 case .search(let key?, let language?):
     let process = ProcessArgs(stringConfig: "search", stringKey: key, stringLanguage: language)
-    try search(languages: &lang, process: process, path: path)
+    try search(languages: &data, process: process) // не передавать весь класс , а передать отдельные аргументы и создать из функций классы с интерфейсами
 case .update(let word, let key, let language):
     let process = ProcessArgs(stringConfig: "update", stringWord: word,stringKey: key, stringLanguage: language)
-    try update(languages: &lang, process: process, path: path)
+    try update(languages: &data, process: process, path: path)
 case .delete(let key?, let language?):
     let process = ProcessArgs(stringConfig: "delete",stringKey: key, stringLanguage: language)
-    try delete(languages: &lang, process: process, path: path)
+    try delete(languages: &data, process: process, path: path)
 default:
     exit(0)
 }*/
