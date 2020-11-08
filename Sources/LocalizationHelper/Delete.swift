ffd61d37335af88c2006630358a98c1716e3d67e
solo <solovey.1999@mail.ru>
2020-11-08 20:18:47 +0700

    Refactor deleting and main func for work with new saving class

--- Sources/LocalizationHelper/Delete.swift
+++ Sources/LocalizationHelper/Delete.swift
@@ -1,29 +1,28 @@
 import Foundation
 
-func delete(languages: inout [Language], process: ProcessArgs, path: String) throws {
+func delete(languages: inout [Language], key: String, language: String, updatingDataClass: SetDataProtocol) throws {
 
     let keys = getKeys(languages: languages)
     let languagesKeys = getLanguagesKeys(languages: languages)
-    if process.language == "" && process.key != "" {
-        deleteWithKey(process: process, keys: keys, languages: &languages)
-    } else if process.language != "" && process.key == "" {
-        deleteWithLanguage(process: process, languagesKeys: languagesKeys, languages: &languages)
-    } else if process.language != "" && process.key != "" {
-        deleteWithAllArg(process: process, languagesKeys: languagesKeys, languages: &languages)
+    if language == "" && key != "" {
+        deleteWithKey(, keys: keys, languages: &languages)
+    } else if language != "" && key == "" {
+        deleteWithLanguage(key: key, language: languages, languagesKeys: languagesKeys, languages: &languages)
+    } else if language != "" && key != "" {
+        deleteWithAllArg(key: key, language: languages, languagesKeys: languagesKeys, languages: &languages)
     }
-    let json = try JSONEncoder().encode(languages.self)
-    try json.write(to: URL(fileURLWithPath: path))
+    try updatingDataClass.settingData(languages: &languages)
 }
 
-func deleteWithKey(process: ProcessArgs, keys: [String], languages: inout [Language]) {
-    guard keys.contains(process.key) else {
+func deleteWithKey(key: String, language: String, keys: [String], languages: inout [Language]) {
+    guard keys.contains(.key) else {
         print("Not Found")
         exit(0)
     }
     for i in 0...languages.count - 1 {
-        if languages[i].words.keys.contains(process.key) {
+        if languages[i].words.keys.contains(key) {
             for (key, _) in languages[i].words
-                where key == process.key {
+                where key == key {
                 languages[i].words.removeValue(forKey: key)
                 print("Word \(key) was deleted from language \(languages[i].key)")
                 break
@@ -32,13 +31,13 @@ func deleteWithKey(process: ProcessArgs, keys: [String], languages: inout [Langu
     }
 }
 
-func deleteWithLanguage(process: ProcessArgs, languagesKeys: [String], languages: inout [Language]) {
-    guard languagesKeys.contains(process.language) else {
+func deleteWithLanguage( key: String, language: String, languagesKeys: [String], languages: inout [Language]) {
+    guard languagesKeys.contains(language) else {
         print("Not Found")
         exit(0)
     }
     for i in 0...languages.count - 1 {
-        if languages[i].key == process.language {
+        if languages[i].key == language {
             print("Language \(languages[i].key) was deleted")
             languages.remove(at: i)
             break
@@ -46,14 +45,14 @@ func deleteWithLanguage(process: ProcessArgs, languagesKeys: [String], languages
     }
 }
 
-func deleteWithAllArg(process: ProcessArgs, languagesKeys: [String], languages: inout [Language]) {
-    guard languagesKeys.contains(process.language) else {
+func deleteWithAllArg( key: String, language: String, languagesKeys: [String], languages: inout [Language]) {
+    guard languagesKeys.contains(language) else {
         print("Not Found")
         exit(0)
     }
     for i in 0...languages.count - 1 {
         for (key, value) in languages[i].words
-            where key == process.key && languages[i].key == process.language {
+            where key == key && languages[i].key == language {
             languages[i].words.removeValue(forKey: key)
             print("Word \(value) was deleted from language \(languages[i].key)")
             break
--- Sources/LocalizationHelper/main.swift
+++ Sources/LocalizationHelper/main.swift
@@ -20,20 +20,17 @@ let arguments = container.argumentParser.parsing()
 let getterData = container.getData
 var data = try getterData.gettingData()
 
-let setterData = container.setData
-
 if case .search(let key, let language) = arguments {
     try search(languages: &data, key: key ?? "", language: language ?? "") // не передавать весь класс , а передать отдельные аргументы и создать из функций классы с интерфейсами
 }
 if case .update(let word, let key, let language) = arguments {
+    let setterData = container.setData
     try update(languages: &data,word: word, key: key ?? "", language: language ?? "", updatingDataClass: setterData )
 }
 if case .delete(let key?, let language?) = arguments {
-    //let process = ProcessArgs(stringConfig: "delete", stringKey: key, stringLanguage: language)
-    //try delete(languages: &data, process: process, path: path)
+    let setterData = container.setData
+    try delete(languages: &data, key: key ?? "", language: language ?? "", updatingDataClass: setterData)
 }
-
-
 
 
 
