01a25184218963a5678098e0064039b7a2d420d6
solo <solovey.1999@mail.ru>
2020-11-08 19:19:00 +0700

    Deleted unnecessary function from protocol

--- Sources/LocalizationHelper/JsonGetter.swift
+++ Sources/LocalizationHelper/JsonGetter.swift
@@ -1,12 +1,13 @@
 import Foundation
 
 class JsonGetter: GetDataProtocol {
+  /*  func gettingData() throws -> [Language] {
+        let path = Bundle.module.path(forResource: "languages", ofType: "json") ?? "languages.json"
+        let languages = try getLanguages(path: path)
+        return languages
+    }*/
     func gettingData() throws -> [Language] {
         let path = Bundle.module.path(forResource: "languages", ofType: "json") ?? "languages.json"
-        let lang = try getJson(path: path)
-        return lang
-    }
-    func getJson(path: String) throws -> [Language] {
         var languages = [] as [Language]
         if let json = FileManager.default.contents(atPath: path) {
             try languages = JSONDecoder().decode([Language].self, from: json)
