b358a7081c30633c546a4cac9b98304daa9675f8
solo <solovey.1999@mail.ru>
2020-11-10 21:39:38 +0700

    Refactor main for work with new class

--- Sources/LocalizationHelper/main.swift
+++ Sources/LocalizationHelper/main.swift
@@ -19,6 +19,9 @@ class Container {
     var delete: DeletingProtocol{
         DeleteData()
     }
+    var getterString: GetStringKeysProtocol{
+        LanguagesKeys()
+    }
 }
 
 let container = Container()
@@ -29,16 +32,18 @@ let arguments = container.argumentParser.parsing()
 let getterData = container.getData
 var data = try getterData.gettingData()
 
+let getterStrings = container.getterString
+
 if case .search(let key, let language) = arguments {
-    try container.search.run(languages: &data, key: key ?? "", language: language ?? "")
+    try container.search.run(languages: &data, key: key ?? "", language: language ?? "", getterStrings: getterStrings)
 }
 if case .update(let word, let key, let language) = arguments {
     let setterData = container.setData
-    try container.update.run(languages: &data,word: word, key: key, language: language, updatingDataClass: setterData )
+    try container.update.run(languages: &data,word: word, key: key, language: language, updatingDataClass: setterData, getterStrings: getterStrings)
 }
 if case .delete(let key, let language) = arguments {
     let setterData = container.setData
-    try container.delete.run(languages: &data, key: key ?? "", language: language ?? "", updatingDataClass: setterData)
+    try container.delete.run(languages: &data, key: key ?? "", language: language ?? "", updatingDataClass: setterData, getterStrings: getterStrings)
 }
 
 
