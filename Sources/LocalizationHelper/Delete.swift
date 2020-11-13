4cfd7daa63aa785468f329758016f838f20cac84
solo <solovey.1999@mail.ru>
2020-11-13 23:14:21 +0700

    Fix naming

--- Sources/LocalizationHelper/Delete.swift
+++ Sources/LocalizationHelper/Delete.swift
@@ -5,7 +5,7 @@ class DeleteData: DeletingProtocol {
         languages[indexValue].words.removeValue(forKey: key)
     }
     func deleteWithLanguage(indexValue: Int, languages: inout [Language]) {
-        languages.remove(at: i)
+        languages.remove(at: indexValue)
     }
     func deleteWithAllArg(indexValue: Int, key: String, languages: inout [Language]) {
         languages[indexValue].words.removeValue(forKey: key)
