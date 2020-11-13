3bb659a2b6e3083ee29192e0f7fb32c637164112
solo <solovey.1999@mail.ru>
2020-11-13 22:58:50 +0700

    Refactoring deleting protocol and function delete with languages

--- Sources/LocalizationHelper/Delete.swift
+++ Sources/LocalizationHelper/Delete.swift
@@ -1,25 +1,12 @@
 import Foundation
 
 class DeleteData: DeletingProtocol {
-
     func deleteWithKey(indexValue: Int, key: String, languages: inout [Language]) {
         languages[indexValue].words.removeValue(forKey: key)
     }
-
-    func deleteWithLanguage(key: String, language: String, languagesKeys: [String], languages: inout [Language]) {
-        guard languagesKeys.contains(language) else {
-            print("Not Found")
-            exit(0)
-        }
-        for i in 0...languages.count - 1 {
-            if languages[i].key == language {
-                print("Language \(languages[i].key) was deleted")
-                languages.remove(at: i)
-                break
-            }
-        }
+    func deleteWithLanguage(indexValue: Int, languages: inout [Language]) {
+        languages.remove(at: i)
     }
-
     func deleteWithAllArg(indexValue: Int, key: String, languages: inout [Language]) {
         languages[indexValue].words.removeValue(forKey: key)
     }
--- Sources/LocalizationHelper/DeletingProtocol.swift
+++ Sources/LocalizationHelper/DeletingProtocol.swift
@@ -3,7 +3,7 @@ import Foundation
 protocol DeletingProtocol {
 
     //func run(languages: inout [Language], key: String, language: String,keys:[String],languagesKeys:[String]) throws
-    func deleteWithKey(indexValue: Int,key: String, languages: inout [Language])
-    func deleteWithLanguage(key: String, language: String, languagesKeys: [String], languages: inout [Language])
+    func deleteWithKey(indexValue: Int, key: String, languages: inout [Language])
+    func deleteWithLanguage(indexValue: Int, languages: inout [Language])
     func deleteWithAllArg(indexValue: Int, key: String, languages: inout [Language])
 }
