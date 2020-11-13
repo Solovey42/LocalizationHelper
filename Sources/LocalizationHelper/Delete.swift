90992ccdec7796442ae0f19ea85e55c9c6fb11f2
solo <solovey.1999@mail.ru>
2020-11-13 21:38:53 +0700

    Optimization deleting class for new protocol

--- Sources/LocalizationHelper/Delete.swift
+++ Sources/LocalizationHelper/Delete.swift
@@ -1,18 +1,6 @@
 import Foundation
 
 class DeleteData: DeletingProtocol {
-    func run(languages: inout [Language], key: String, language: String, updatingDataClass: SetDataProtocol, getterStrings: GetStringKeysProtocol) throws {
-        let keys = getterStrings.getKeys(languages: languages)
-        let languagesKeys = getterStrings.getLanguagesKeys(languages: languages)
-        if language == "" && key != "" {
-            deleteWithKey(key: key, language: language, keys: keys, languages: &languages)
-        } else if language != "" && key == "" {
-            deleteWithLanguage(key: key, language: language, languagesKeys: languagesKeys, languages: &languages)
-        } else if language != "" && key != "" {
-            deleteWithAllArg(key: key, language: language, languagesKeys: languagesKeys, languages: &languages)
-        }
-        try updatingDataClass.settingData(languages: &languages)
-    }
 
     func deleteWithKey(key: String, language: String, keys: [String], languages: inout [Language]) {
         guard keys.contains(key) else {
@@ -45,18 +33,7 @@ class DeleteData: DeletingProtocol {
         }
     }
 
-    func deleteWithAllArg(key: String, language: String, languagesKeys: [String], languages: inout [Language]) {
-        guard languagesKeys.contains(language) else {
-            print("Not Found")
-            exit(0)
-        }
-        for i in 0...languages.count - 1 {
-            for (wordKey, value) in languages[i].words
-                where wordKey == key && languages[i].key == language {
-                languages[i].words.removeValue(forKey: key)
-                print("Word \(value) was deleted from language \(languages[i].key)")
-                break
-            }
-        }
+    func deleteWithAllArg(key: String, languages: inout [Language]) {
+        languages[i].words.removeValue(forKey: key)
     }
 }
