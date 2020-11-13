f884a4bde625a52d82daee4c49a8ec67466623c1
solo <solovey.1999@mail.ru>
2020-11-13 15:16:51 +0700

    Added error throw when saving data failed

--- Sources/LocalizationHelper/SettingData/JsonSetter.swift
+++ Sources/LocalizationHelper/SettingData/JsonSetter.swift
@@ -1,9 +1,13 @@
 import Foundation
 
-class JsonSetter: SetDataProtocol{
+class JsonSetter: SetDataProtocol {
     func settingData(languages: inout [Language]) throws {
         let path = Bundle.module.path(forResource: "languages", ofType: "json") ?? "languages.json"
-        let json = try JSONEncoder().encode(languages.self)
-        try json.write(to: URL(fileURLWithPath: path))
+        do {
+            let json = try JSONEncoder().encode(languages.self)
+            try json.write(to: URL(fileURLWithPath: path))
+        } catch {
+            print("Ошибка сохранения данных")
+        }
     }
 }
