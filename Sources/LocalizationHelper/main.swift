7abe01d78193006da05c9ecd4bd6aac9f296fd16
solo <solovey.1999@mail.ru>
2020-11-13 15:40:35 +0700

    Changed enumeration of values through a switch

--- Sources/LocalizationHelper/main.swift
+++ Sources/LocalizationHelper/main.swift
@@ -7,19 +7,19 @@ class Container {
     var getData: GetDataProtocol {
         JsonGetter()
     }
-    var setData: SetDataProtocol{
+    var setData: SetDataProtocol {
         JsonSetter()
     }
-    var search: SearchingProtocol{
+    var search: SearchingProtocol {
         SearchData()
     }
-    var update: UpdatingProtocol{
+    var update: UpdatingProtocol {
         UpdateData()
     }
-    var delete: DeletingProtocol{
+    var delete: DeletingProtocol {
         DeleteData()
     }
-    var getterString: GetStringKeysProtocol{
+    var getterString: GetStringKeysProtocol {
         LanguagesKeys()
     }
 }
@@ -34,17 +34,15 @@ var data = try getterData.gettingData()
 
 let getterStrings = container.getterString
 
-if case .search(let key, let language) = arguments {
+switch (arguments) {
+case .search(let key, let language):
     try container.search.run(languages: &data, key: key ?? "", language: language ?? "", getterStrings: getterStrings)
-}
-if case .update(let word, let key, let language) = arguments {
+case .update(let word, let key, let language):
     let setterData = container.setData
-    try container.update.run(languages: &data,word: word, key: key, language: language, updatingDataClass: setterData, getterStrings: getterStrings)
-}
-if case .delete(let key, let language) = arguments {
+    try container.update.run(languages: &data, word: word, key: key, language: language, updatingDataClass: setterData, getterStrings: getterStrings)
+case .delete(let key, let language):
     let setterData = container.setData
     try container.delete.run(languages: &data, key: key ?? "", language: language ?? "", updatingDataClass: setterData, getterStrings: getterStrings)
+default: break
 }
-
-
 
