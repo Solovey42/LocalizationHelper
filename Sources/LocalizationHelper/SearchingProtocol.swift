d2a346ae8476f6f738c9b0eabfaea26daa66259f
solo <solovey.1999@mail.ru>
2020-11-13 21:39:23 +0700

    Refactor searching protocol for new logic

--- Sources/LocalizationHelper/SearchingProtocol.swift
+++ Sources/LocalizationHelper/SearchingProtocol.swift
@@ -1,5 +1,18 @@
 import Foundation
 
 protocol SearchingProtocol {
-    func run(languages: inout [Language],key: String, language: String, getterStrings: GetStringKeysProtocol) throws
+    var command: String { get set }
+    var languages: [Language] { get set }
+    var key: String? { get set }
+    var language: String? { get set }
+    var word: String? { get set }
+    var gettingDataClass: GetDataProtocol? { get set }
+    var updatingDataClass: SetDataProtocol? { get set }
+    var getterStrings: GetStringKeysProtocol? { get set }
+    var deleterClass: DeletingProtocol? { get set }
+
+    func run(command: String,key: String, language: String, word:String, gettingDataClass: GetDataProtocol, updatingDataClass: SetDataProtocol, getterStrings: GetStringKeysProtocol, deleterClass:DeletingProtocol) throws
+/*    func search()
+    func update()
+    func delete() throws*/
 }
