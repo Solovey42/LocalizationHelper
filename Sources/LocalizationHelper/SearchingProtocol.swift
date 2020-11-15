f04348e5c312860a4b55776b0b47f5becd07c0e2
solo <solovey.1999@mail.ru>
2020-11-16 04:28:55 +0700

    Removing unnecessary dependencies from the search protocol.

--- Sources/LocalizationHelper/SearchingProtocol.swift
+++ Sources/LocalizationHelper/SearchingProtocol.swift
@@ -1,17 +1,5 @@
 import Foundation
 
 protocol SearchingProtocol {
-    var command: String { get set }
-    var languages: [Language] { get set }
-    var key: String { get set }
-    var language: String { get set }
-    var word: String { get set }
-    var gettingDataClass: GetDataProtocol { get set }
-    var updatingDataClass: SetDataProtocol { get set }
-    var getterStrings: GetStringKeysProtocol { get set }
-    var deleterClass: DeletingProtocol { get set }
-    var updaterClass: UpdatingProtocol { get set }
-    var outputClass: OutputProtocol { get set }
-
     func search(command: String, key: String, language: String, word: String) throws
 }
