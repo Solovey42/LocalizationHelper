4028f17780354b9428a6f8c27b0918016a39c79e
solo <solovey.1999@mail.ru>
2020-11-16 03:16:07 +0700

    Creating protocol and class with functions for printing

---
+++ Sources/LocalizationHelper/OutputProtocol.swift
@@ -0,0 +1,14 @@
+//
+// Created by solo on 16.11.2020.
+//
+
+import Foundation
+
+protocol OutputProtocol {
+    func printWithAllArg(key: String, value: String)
+    func printWord(word: String)
+    func printUpdate(value:String)
+    func printDeleteLanguage(value: String)
+    func printDeleteWord(key: String, value: String)
+    func printNotFound()
+}
