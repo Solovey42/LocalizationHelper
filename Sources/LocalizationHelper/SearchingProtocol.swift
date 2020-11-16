aba5d3854b4338aee934239c1972467a05168c53
solo <solovey.1999@mail.ru>
2020-11-17 00:01:18 +0700

    Fix return values

--- Sources/LocalizationHelper/SearchingProtocol.swift
+++ Sources/LocalizationHelper/SearchingProtocol.swift
@@ -3,8 +3,8 @@ import Foundation
 protocol SearchingProtocol {
     var outputClass: OutputProtocol { get set }
     //func search(command: String, key: String, language: String, word: String) throws
-    func searchWithOutArg(keys: [String], languages: [Language]) -> (key: String, value: String)?
+    func searchWithOutArg(keys: [String], languages: [Language]) -> [(key: String, languageKey: String, value: String)]
     func searchWithLanguage(languagesKeys: [String], language: String, languages: [Language]) ->  Int?
-    func searchWithKey(keys: [String], key: String, languages: [Language]) -> [(indexValue: Int, key: String, value: String)]
+    func searchWithKey(keys: [String], key: String, languages: [Language]) -> [(indexValue: Int, key: String, value: String)]?
     func searchWitAllArg(languagesKeys: [String], language: String, languages: [Language], key: String, word: String) -> (indexValue: Int, word: String, key: String, value: String)?
 }
