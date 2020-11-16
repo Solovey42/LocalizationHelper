7d99f7a613bb2cf513ced6f7830347f1e5b5d135
solo <solovey.1999@mail.ru>
2020-11-17 00:01:49 +0700

    Deleting unnecessary dependencies

--- Sources/LocalizationHelper/DeletingProtocol.swift
+++ Sources/LocalizationHelper/DeletingProtocol.swift
@@ -2,7 +2,4 @@ import Foundation
 
 protocol DeletingProtocol {
     func startDeleting( key: String, language: String, word: String) throws
-    func deleteWithKey(items: [(indexValue: Int, key: String, value: String)])
-    func deleteWithLanguage(indexLanguage: Int?)
-    func deleteWithAllArg(item: (indexValue: Int, word: String, key: String, value: String)?)
 }
