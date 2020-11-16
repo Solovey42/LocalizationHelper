658050243448996dafb850e86ec06ef92eadb10b
solo <solovey.1999@mail.ru>
2020-11-16 22:10:50 +0700

    Refactoring search class

--- Sources/LocalizationHelper/Search.swift
+++ Sources/LocalizationHelper/Search.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 class SearchData: SearchingProtocol {
-    var command: String = ""
+/*    var command: String = ""
     var languages: [Language] = []
     var key: String = ""
     var language: String = ""
@@ -12,110 +12,126 @@ class SearchData: SearchingProtocol {
     var keys: [String] = []
     var languagesKeys: [String] = []
     var deleterClass: DeletingProtocol
-    var updaterClass: UpdatingProtocol
+    var updaterClass: UpdatingProtocol*/
     var outputClass: OutputProtocol
 
-    init(gettingDataClass: GetDataProtocol, updatingDataClass: SetDataProtocol, getterStrings: GetStringKeysProtocol, deleterClass: DeletingProtocol, updaterClass: UpdatingProtocol, outputClass: OutputProtocol) {
-        self.gettingDataClass = gettingDataClass
-        self.updatingDataClass = updatingDataClass
-        self.getterStrings = getterStrings
-        self.deleterClass = deleterClass
-        self.updaterClass = updaterClass
+    init(outputClass: OutputProtocol) {
+
         self.outputClass = outputClass
+
     }
 
-    func search(command: String, key: String, language: String, word: String) throws {
-        self.command = command
-        self.languages = try gettingDataClass.gettingData()
-        self.key = key
-        self.language = language
-        self.word = word
-        self.keys = getterStrings.getKeys(languages: languages)
-        self.languagesKeys = getterStrings.getLanguagesKeys(languages: languages)
+    /*   var selectorClass: ActionSelectionClass
 
 
-        if language == "" && key == "" {
-            searchWithOutArg()
-        } else if language == "" && key != "" {
-            searchWithKey()
-            if command == "delete" {
-                try updatingDataClass.settingData(languages: &languages)
-            }
-        } else if language != "" && key == "" {
-            searchWithLanguage()
-            if command == "delete" {
-                try updatingDataClass.settingData(languages: &languages)
-            }
-        } else {
-            searchWitAllArg()
-            if command == "delete" || command == "update" {
-                try updatingDataClass.settingData(languages: &languages)
-            }
-        }
-    }
 
-    func searchWithOutArg() {
+      func search(command: String, key: String, language: String, word: String) throws {
+          self.command = command
+          self.languages = try gettingDataClass.gettingData()
+          self.key = key
+          self.language = language
+          self.word = word
+          self.keys = getterStrings.getKeys(languages: languages)
+          self.languagesKeys = getterStrings.getLanguagesKeys(languages: languages)
+
+          if language == "" && key == "" {
+              searchWithOutArg()
+          } else if language == "" && key != "" {
+              searchWithKey()
+              if command == "delete" {
+                  try updatingDataClass.settingData(languages: &languages)
+              }
+          } else if language != "" && key == "" {
+              searchWithLanguage()
+              if command == "delete" {
+                  try updatingDataClass.settingData(languages: &languages)
+              }
+          } else {
+              searchWitAllArg()
+              if command == "delete" || command == "update" {
+                  try updatingDataClass.settingData(languages: &languages)
+              }
+          }
+      }*/
+
+    func searchWithOutArg(keys: [String], languages: [Language]) -> (key: String, value: String)? {
         for item in keys {
             outputClass.printWord(word: item)
             for n in 0...languages.count - 1 {
                 for (key, value) in languages[n].words
                     where item == key {
+                    return (languages[n].key, value)
                     outputClass.printWithAllArg(key: languages[n].key, value: value)
                 }
             }
         }
+        return nil
     }
 
-    func searchWithLanguage() {
+    func searchWithLanguage(languagesKeys: [String], language: String, languages: [Language]) -> Int?{
         guard languagesKeys.contains(language) else {
             outputClass.printNotFound()
             exit(0)
         }
         for i in 0...languages.count - 1 {
             if languages[i].key == language {
-                if command == "search" {
-                    for (key, value) in languages[i].words {
-                        outputClass.printWithAllArg(key: key, value: value)
-                    }
-                } else if command == "delete" {
-                    deleterClass.deleteWithLanguage(indexValue: i, languages: &languages)
-                    outputClass.printDeleteLanguage(value: language)
-                    break
-                }
+                return i
             }
+/*            if command == "search" {
+                for (key, value) in languages[i].words {
+
+                    //array.append((i, key, value))
+                    //   outputClass.printWithAllArg(key: key, value: value)
+                }
+                               } else if command == "delete" {
+                                   deleterClass.deleteWithLanguage(indexValue: i, languages: &languages)
+                                   outputClass.printDeleteLanguage(value: language)
+                                   break
+
+            }*/
         }
+        return nil
     }
 
-
-    func searchWithKey() {
+    func searchWithKey(keys: [String], key: String, languages: [Language]) -> [(indexValue: Int, key: String, value: String)] {
         guard keys.contains(key) else {
             outputClass.printNotFound()
             exit(0)
         }
-        outputClass.printWord(word: key)
+        //outputClass.printWord(word: key)
+        var array: [(Int, String, String)] = []
         for i in 0...languages.count - 1 {
             if languages[i].words.keys.contains(key) {
                 for (wordKey, value) in languages[i].words
                     where wordKey == key {
-                    if command == "search" {
+                    array.append((i, key, value))
+/*                    if command == "search" {
                         outputClass.printWithAllArg(key: languages[i].key, value: value)
                         break
                     } else if command == "delete" {
                         deleterClass.deleteWithKey(indexValue: i, key: key, languages: &languages)
                         outputClass.printDeleteWord(key: key, value: languages[i].key)
                         break
-                    }
+                    }*/
                 }
             }
         }
+        return array
     }
 
-    func searchWitAllArg() {
+    func searchWitAllArg(languagesKeys: [String], language: String, languages: [Language], key: String, word: String) -> (indexValue: Int, word: String, key: String, value: String)? {
+        print(languagesKeys)
+        print(language)
+        print(languages)
+        print(key)
+        print(word)
         guard !languagesKeys.contains(language) else {
             for i in 0...languages.count - 1 {
                 for (wordKey, value) in languages[i].words
                     where wordKey == key && languages[i].key == language {
-                    if command == "search" {
+                    print((i,word,key,value))
+                    return (i, word, key, value)
+/*                    if command == "search" {
                         outputClass.printWord(word: value)
                         break
                     } else if command == "delete" {
@@ -125,13 +141,13 @@ class SearchData: SearchingProtocol {
                     } else if command == "update" {
                         updaterClass.update(indexValue: i, word: word, key: key, languages: &languages)
                         outputClass.printUpdate(value: value)
-
-                    }
+                    }*/
                 }
             }
-            return
+            return nil
         }
         outputClass.printNotFound()
+        return nil
     }
 
 }
--- Sources/LocalizationHelper/SearchingProtocol.swift
+++ Sources/LocalizationHelper/SearchingProtocol.swift
@@ -1,5 +1,10 @@
 import Foundation
 
 protocol SearchingProtocol {
-    func search(command: String, key: String, language: String, word: String) throws
+    var outputClass: OutputProtocol { get set }
+    //func search(command: String, key: String, language: String, word: String) throws
+    func searchWithOutArg(keys: [String], languages: [Language]) -> (key: String, value: String)?
+    func searchWithLanguage(languagesKeys: [String], language: String, languages: [Language]) ->  Int?
+    func searchWithKey(keys: [String], key: String, languages: [Language]) -> [(indexValue: Int, key: String, value: String)]
+    func searchWitAllArg(languagesKeys: [String], language: String, languages: [Language], key: String, word: String) -> (indexValue: Int, word: String, key: String, value: String)?
 }
