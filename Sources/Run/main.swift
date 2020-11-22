0757f52d35dd10eee30be3429a8f103af553939c
solo <solovey.1999@mail.ru>
2020-11-22 16:57:41 +0700

    Refactored main for working with result.

--- Sources/Run/main.swift
+++ Sources/Run/main.swift
@@ -1,5 +1,28 @@
 import Foundation
 import LocalizationHelper
-let exitCode = app().hashValue
-exit(Int32(exitCode))
+
+let exitCode: Int32
+switch app() {
+case .Success:
+    exitCode = 0
+case .HelpCode:
+    exitCode = 1
+case .UnknownLanguage:
+    exitCode = 2
+case .UnknownKey:
+    exitCode = 3
+case .UnknownWord:
+    exitCode = 4
+case .WriteError:
+    exitCode = 5
+case .ReadError:
+    exitCode = 6
+case .ShowError:
+    exitCode = 7
+case .DeleteError:
+    exitCode = 8
+case .UpdateError:
+    exitCode = 9
+}
+exit(exitCode)
 
