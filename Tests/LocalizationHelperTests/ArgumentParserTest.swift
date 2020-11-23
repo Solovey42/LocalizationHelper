9378faa0a74efa105ad04cfedea2b4c6b6ce795c
solo <solovey.1999@mail.ru>
2020-11-23 20:31:20 +0700

    Added failed cases to unit-tests for ArgumentParser

--- Tests/LocalizationHelperTests/ArgumentParserTest.swift
+++ Tests/LocalizationHelperTests/ArgumentParserTest.swift
@@ -20,7 +20,7 @@ class ArgumentParserTest: XCTestCase {
         XCTAssertEqual(actualKey, expectedKey)
         XCTAssertEqual(actualLanguage, expectedLanguage)
     }
-    
+
     func testParsingDelete() throws {
         let actualResult = parser.parsing(["delete", "-l", "rus", "-k", "ddd"])
         let expectedKey = "ddd"
@@ -33,7 +33,7 @@ class ArgumentParserTest: XCTestCase {
         XCTAssertEqual(actualKey, expectedKey)
         XCTAssertEqual(actualLanguage, expectedLanguage)
     }
-    
+
     func testParsingUpdate() throws {
         let actualResult = parser.parsing(["update", "123", "-l", "rus", "-k", "ddd"])
         let expectedWord = "123"
@@ -48,7 +48,7 @@ class ArgumentParserTest: XCTestCase {
         XCTAssertEqual(actualKey, expectedKey)
         XCTAssertEqual(actualLanguage, expectedLanguage)
     }
-    
+
     func testParsingSearchKey() throws {
         let actualResult = parser.parsing(["search", "-k", "ddd"])
         let expectedKey = "ddd"
@@ -60,7 +60,7 @@ class ArgumentParserTest: XCTestCase {
         XCTAssertEqual(actualKey, expectedKey)
         XCTAssertEqual(actualLanguage, nil)
     }
-    
+
     func testParsingDeleteLanguage() throws {
         let actualResult = parser.parsing(["delete", "-l", "rus"])
         let expectedLanguage = "rus"
@@ -73,11 +73,55 @@ class ArgumentParserTest: XCTestCase {
         XCTAssertEqual(actualLanguage, expectedLanguage)
     }
 
+    func testParsingSearchLanguage() throws {
+        let actualResult = parser.parsing(["search", "-l", "rus"])
+        let expectedLanguage = "rus"
+
+        guard case .search(let actualKey, let actualLanguage) = actualResult else {
+            XCTFail("Invalid command. Expected command 'search'")
+            return
+        }
+        XCTAssertEqual(actualKey, nil)
+        XCTAssertEqual(actualLanguage, expectedLanguage)
+    }
+
+    func testParsingDeleteKey() throws {
+        let actualResult = parser.parsing(["delete", "-k", "ddd"])
+        let expectedKey = "ddd"
+
+        guard case .delete(let actualKey, let actualLanguage) = actualResult else {
+            XCTFail("Invalid command. Expected command 'search'")
+            return
+        }
+        XCTAssertEqual(actualKey, expectedKey)
+        XCTAssertEqual(actualLanguage, nil)
+    }
+
+    func testParsingUpdateError() throws {
+        let actualResult = parser.parsing(["update", "-l", "rus", "-k", "ddd"])
+        XCTAssert(actualResult == nil)
+    }
+
+    func testParsingDeleteError() throws {
+        let actualResult = parser.parsing(["search", "123", "-l", "rus", "-k", "ddd"])
+        XCTAssert(actualResult == nil)
+    }
+
+    func testParsingSearchError() throws {
+        let actualResult = parser.parsing(["search", "123", "-l", "rus", "-k", "ddd"])
+        XCTAssert(actualResult == nil)
+    }
+
     static var allTests = [
         ("testParsingSearch", testParsingSearch),
-        ("testParsingDelete",testParsingDelete),
-        ("testParsingUpdate",testParsingUpdate),
-        ("testParsingSearchKey",testParsingSearchKey),
-        ("testParsingDeleteLanguage",testParsingDeleteLanguage)
+        ("testParsingDelete", testParsingDelete),
+        ("testParsingUpdate", testParsingUpdate),
+        ("testParsingSearchKey", testParsingSearchKey),
+        ("testParsingDeleteLanguage", testParsingDeleteLanguage),
+        ("testParsingSearchLanguage", testParsingSearchLanguage),
+        ("testParsingDeleteKey", testParsingDeleteKey),
+        ("testParsingUpdateError", testParsingUpdateError),
+        ("testParsingDeleteError", testParsingDeleteError),
+        ("testParsingSearchError", testParsingSearchError)
     ]
 }
