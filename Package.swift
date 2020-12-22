fdad847feed412cd3ae6cc4fdd4c37539a30b4a3
solo <solovey.1999@mail.ru>
2020-12-22 20:52:44 +0700

    Add Leaf library into dependencies

--- Package.swift
+++ Package.swift
@@ -14,6 +14,7 @@ let package = Package(
             .package(url: "https://github.com/apple/swift-argument-parser", .upToNextMinor(from: "0.3.0")),
             .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
             .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0"),
+            .package(url: "https://github.com/vapor/leaf.git", from: "4.0.0"),
             .package(url: "https://github.com/vapor/fluent-postgres-driver.git", from: "2.0.0")
         ],
         targets: [
@@ -21,7 +22,7 @@ let package = Package(
             // Targets can depend on other targets in this package, and on products in packages this package depends on.
             .target(
                     name: "LocalizationHelper",
-                    dependencies: [.product(name: "ArgumentParser", package: "swift-argument-parser"), .product(name: "Vapor", package: "vapor"), .product(name: "Fluent", package: "fluent"),.product(name: "FluentPostgresDriver", package: "fluent-postgres-driver")],
+                    dependencies: [.product(name: "ArgumentParser", package: "swift-argument-parser"), .product(name: "Vapor", package: "vapor"), .product(name: "Fluent", package: "fluent"),.product(name: "Leaf", package: "leaf"),.product(name: "FluentPostgresDriver", package: "fluent-postgres-driver")],
                     resources: [.process("languages.json")]),
             .target(
                     name: "Run",
